b19d47cb87dcce238d7b7391874c071ab2f2cfcd
JASONMANUSELER <jasonnotuseless168@gmail.com>
2025-04-15 08:47:24 +0700

    Add files via upload

---
+++ src/main/java/jason/modid/NoNetheriteGear.java
@@ -0,0 +1,18 @@
+package jason.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoNetheriteGear implements ModInitializer {
+	public static final String MOD_ID = "nonetheritegear";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/jason/modid/NoNetheriteGearClient.java
@@ -0,0 +1,10 @@
+package jason.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoNetheriteGearClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
